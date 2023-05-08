a81caa7bb5312167805a80b88797ee71873afbf9
Frank <thantsithu250703@gmail.com>
2023-05-08 21:39:30 +0630

    links Changed

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
-import { menu_Tea } from "./Data.js";
+// import { menu_Tea } from "./Data.js";
 
-const testww = document.querySelector('.testing')
-console.log('wtffff');
-console.log(testww);
-
-
+// const testww = document.querySelector('.testing')
+// console.log('wtffff');
+// console.log(testww);
 
 
-const diplay = (arr)=>{
-    arr.forEach((item)=>{
-    const newItemName = document.createElement('p')
-    const newPrice = document.createElement('code')
-    newPrice.innerText = item.Price;
-    newItemName.innerText = item.Title;
-    testww.appendChild(newItemName);
-    testww.appendChild(newPrice);
- })
-}
-diplay(menu_Tea);
 
-const  filter = (menu)=>{
- let newMenu = menu_Tea.filter((item)=> item.category === menu);
- testww.innerHTML ="";
-diplay(newMenu);
- console.log(newMenu);
-}
 
-// const filterTea = (menu) =>{
-//     let newMenu = menu_Tea.filter((item)=> item.category === menu)
+// const diplay = (arr)=>{
+//     arr.forEach((item)=>{
+//     const newItemName = document.createElement('p')
+//     const newPrice = document.createElement('code')
+//     newPrice.innerText = item.Price;
+//     newItemName.innerText = item.Title;
+//     testww.appendChild(newItemName);
+//     testww.appendChild(newPrice);
+//  })
 // }
-const reset = (menu) =>{
-    let newMenu = menu_Tea
-    testww.innerHTML = '';
-    diplay(newMenu)
+// diplay(menu_Tea);
+
+// const  filter = (menu)=>{
+//  let newMenu = menu_Tea.filter((item)=> item.category === menu);
+//  testww.innerHTML ="";
+// diplay(newMenu);
+//  console.log(newMenu);
+// }
+
+// // const filterTea = (menu) =>{
+// //     let newMenu = menu_Tea.filter((item)=> item.category === menu)
+// // }
+// const reset = (menu) =>{
+//     let newMenu = menu_Tea
+//     testww.innerHTML = '';
+//     diplay(newMenu)
+// }
+
+// let btnCoffee = document.querySelector('.btn-coffee')
+// let btnReset = document.querySelector('.btn-reset')
+// let btnTea = document.querySelector('.btn-tea')
+
+// btnCoffee.addEventListener('click',()=>{ filter('Frappuccino')})
+// btnReset.addEventListener('click',()=>{ reset()})
+// btnTea.addEventListener('click',()=>{ filter('Tea')})
+
+
+const curr = location.href;
+const links = document.querySelectorAll('.nav-links');
+// console.log(typeof links[1].href);
+for( let i = 0; i < links.length; i++){
+    if( links[i].href == curr ){
+        links[i].classList.add('active-link')
+    }
 }
 
-let btnCoffee = document.querySelector('.btn-coffee')
-let btnReset = document.querySelector('.btn-reset')
-let btnTea = document.querySelector('.btn-tea')
-
-btnCoffee.addEventListener('click',()=>{ filter('Frappuccino')})
-btnReset.addEventListener('click',()=>{ reset()})
-btnTea.addEventListener('click',()=>{ filter('Tea')})
 
